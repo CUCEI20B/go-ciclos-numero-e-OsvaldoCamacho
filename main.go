59fb0d4ae6700e38c8b7a88839d341956910b458
Osvaldo Camacho <luis.cplascencia@alumnos.udg.mx>
2020-09-29 12:34:13 -0500

    Prueba 01

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n float64
+	var e float64
+
+	fmt.Scan(&n)
+	var auxE float64 = 1
+	var factorial float64 = 1
+	var aux float64 = 1
+	var i float64
+	for i = 1; i <= n; i++ {
+		factorial = aux * i
+		aux = factorial
+		e = (1.0 / factorial) + auxE
+		auxE = e
+
+	}
+	fmt.Println(e)
 }
